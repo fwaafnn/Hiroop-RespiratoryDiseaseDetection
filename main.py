f3d6f77cf42b362b85fcbe288ed9020f732cb21a
fwaafnn <afwaafini.xiia4@gmail.com>
2024-11-11 12:14:08 +0700

    add main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# from ...
+from predict import AudioPredictor
 
 if __name__ == "__main__":
     # Model config
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     audio_path = 'Test/1. Bronchiectasis/116_1b2_Pl_sc_Meditron.wav'
     result = predictor.predict_audio(audio_path)
     
-    print("{audio_label}")
+    print(f"This model predicts the audio as: {audio_result['audio_label']} with prediction scores: {audio_result['prediction_score']}")
